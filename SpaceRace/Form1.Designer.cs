276503c450bad3f5a19197490976b5d9e03cb372
Matt Smith <mattsmit085@outlook.com>
2021-03-22 12:02:16 -0400

    Add project files.

---
+++ SpaceRace/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace SpaceRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-6, 174);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(805, 46);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "SPACE RACE";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(0, 220);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(799, 21);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "Space - Start || Esc - Quit";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 50;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(353, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 22);
+            this.timeLabel.TabIndex = 2;
+            this.timeLabel.Text = "Time: ";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(-3, 9);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(100, 22);
+            this.p1ScoreLabel.TabIndex = 3;
+            this.p1ScoreLabel.Text = "Score:";
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(686, 9);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(100, 22);
+            this.p2ScoreLabel.TabIndex = 5;
+            this.p2ScoreLabel.Text = "Score:";
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::SpaceRace.Properties.Resources.galaxy;
+            this.ClientSize = new System.Drawing.Size(784, 451);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+    }
+}
+
---
+++ SpaceRace/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceRace
+{
+    public partial class Form1 : Form
+    {
+        string scene = "title";
+
+        int heroX = 280;
+        int heroY = 540;
+        int heroWidth = 40;
+        int heroHeight = 10;
+        int heroSpeed = 10;
+
+        List<int> rockXList = new List<int>();
+        List<int> rockYList = new List<int>();
+        List<int> rockSpeedList = new List<int>();
+        int ballSize = 5;
+
+        int score = 0;
+        int time = 1000;
+
+        bool leftDown = false;
+        bool rightDown = false;
+        bool spaceDown = false;
+        bool escapeDown = false;
+
+        string winner = "";
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush goldBrush = new SolidBrush(Color.Gold);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            scene = "run";
+            gameTimer.Enabled = true;
+            heroX = 280;
+            heroY = 540;
+            score = 0;
+            time = 1000;
+            titleLabel.Text = "";
+            subtitleLabel.Text = $"";
+
+        }
+        public void GameTitle()
+        {
+            scene = "title";
+            gameTimer.Enabled = false;
+            heroX = 0;
+            heroY = 0;
+            score = 0;
+            time = 1000;
+
+            titleLabel.Text = "SPACE RACE";
+            subtitleLabel.Text = $"Space - Start || Esc - Quit";
+        }
+        public void GameEnd()
+        {
+            scene = "end";
+            gameTimer.Enabled = false;
+            heroX = 0;
+            heroY = 0;
+            score = 0;
+            time = 1000;
+
+            titleLabel.Text = "GAME OVER";
+            subtitleLabel.Text = $"{winner} Wins!";
+
+        }
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            timeLabel.Text = $"{time}";
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    spaceDown = true;
+                    if (scene == "title" || scene == "end") { GameInitialize(); }
+                    break;
+                case Keys.Escape:
+                    escapeDown = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //update labels 
+
+            timeLabel.Text = $"{time}";
+
+            p1ScoreLabel.Text = $"Score: {score}";
+            p2ScoreLabel.Text = $"Score: {score}";
+            if (scene == "run")
+            {
+                e.Graphics.FillRectangle(whiteBrush, 400, 40, 5, 490);
+            }
+
+
+        }
+    }
+}
